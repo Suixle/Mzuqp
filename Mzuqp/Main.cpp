c3a3d0498fe07481deb2b04335fade79abcd5d01
Suixle <deverewb@gmail.com>
2016-04-29 10:59:05 -0400

    Added Comments

--- Mzuqp/Main.cpp
+++ Mzuqp/Main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <SFML\Graphics.hpp>
 
-
-
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(400, 400), "Mzuqp");
 
 	sf::Texture shop; shop.loadFromFile("shop.png");
-	sf::Sprite spr; spr.setScale(3, 3); spr.setColor(sf::Color::Color(50, 50, 50, 255));
+	sf::Sprite spr; spr.setTexture(shop); spr.setScale(3, 3); spr.setColor(sf::Color::Color(50, 50, 50, 255));
 	while (window.isOpen())
 	{ 
 		window.clear(sf::Color::Color(0, 255, 255, 255));
